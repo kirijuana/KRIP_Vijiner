4ed7bc52ab86a4298b0d754926208238ba698945
JussyFrussy <sazkirand@mail.ru>
2019-04-19 12:55:25 +0700

    Добавьте файлы проекта.

---
+++ KRIP_Vijiner/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace KRIP_Vijiner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Shifr = new System.Windows.Forms.Button();
+            this.button_unShifr = new System.Windows.Forms.Button();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.Text = new System.Windows.Forms.TextBox();
+            this.ShifrText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Shifr
+            // 
+            this.button_Shifr.Location = new System.Drawing.Point(283, 151);
+            this.button_Shifr.Name = "button_Shifr";
+            this.button_Shifr.Size = new System.Drawing.Size(124, 23);
+            this.button_Shifr.TabIndex = 0;
+            this.button_Shifr.Text = "Зашифровать";
+            this.button_Shifr.UseVisualStyleBackColor = true;
+            this.button_Shifr.Click += new System.EventHandler(this.button_Shifr_Click);
+            // 
+            // button_unShifr
+            // 
+            this.button_unShifr.Location = new System.Drawing.Point(283, 194);
+            this.button_unShifr.Name = "button_unShifr";
+            this.button_unShifr.Size = new System.Drawing.Size(124, 23);
+            this.button_unShifr.TabIndex = 1;
+            this.button_unShifr.Text = "Расшифровать";
+            this.button_unShifr.UseVisualStyleBackColor = true;
+            this.button_unShifr.Click += new System.EventHandler(this.button_unShifr_Click);
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(60, 173);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(189, 20);
+            this.keyBox.TabIndex = 2;
+            // 
+            // Text
+            // 
+            this.Text.Location = new System.Drawing.Point(108, 87);
+            this.Text.Name = "Text";
+            this.Text.Size = new System.Drawing.Size(456, 20);
+            this.Text.TabIndex = 3;
+            this.Text.TextChanged += new System.EventHandler(this.Text_TextChanged);
+            // 
+            // ShifrText
+            // 
+            this.ShifrText.Location = new System.Drawing.Point(108, 267);
+            this.ShifrText.Name = "ShifrText";
+            this.ShifrText.Size = new System.Drawing.Size(456, 20);
+            this.ShifrText.TabIndex = 4;
+            this.ShifrText.TextChanged += new System.EventHandler(this.ShifrText_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 406);
+            this.Controls.Add(this.ShifrText);
+            this.Controls.Add(this.Text);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.button_unShifr);
+            this.Controls.Add(this.button_Shifr);
+            this.Name = "Form1";
+       //     this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Shifr;
+        private System.Windows.Forms.Button button_unShifr;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.TextBox Text;
+        private System.Windows.Forms.TextBox ShifrText;
+    }
+}
+
---
+++ KRIP_Vijiner/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KRIP_Vijiner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Shifr_Click(object sender, EventArgs e)
+        {
+            char[] text = Text.Text.ToCharArray();
+            char[] shifr = Text.Text.ToCharArray();
+            char[] ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char[] alpha = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            char[] alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+            char[] ALPH = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray();
+            char[] key = keyBox.Text.ToCharArray();
+
+            int text_len = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < ALPH.Length; j++)
+                {
+                    if (text[i] == ALPH[j] || text[i] == alph[j])
+                        text_len++;
+                }
+
+                for (int j = 0; j < ALPHA.Length; j++)
+                {
+                    if (text[i] == ALPHA[j] || text[i] == alpha[j])
+                        text_len++;
+                }
+            }
+
+            char[] full_key = new char[text_len];
+            int i1 = 0, j1 = 0;
+            for (int i = 0; i < text_len;) // дополняем ключ с проверкой на посторонние символы
+            {
+                for (int j = 0; j < ALPH.Length; j++)
+                {
+                    if (key[i1] == ALPH[j] || key[i1] == alph[j])
+                    {
+                        full_key[i] = key[i1];
+                        i++;
+                        break;
+                    }
+                }
+                for (int j = 0; j < ALPHA.Length; j++)
+                {
+                    if (key[i1] == ALPHA[j] || key[i1] == alpha[j])
+                    {
+                        full_key[i] = key[i1];
+                        i++;
+                        break;
+                    }
+                }
+                i1++;
+                if (i1 == key.Length)
+                    i1 = 0;
+            }
+
+            int k = 0, z = 0; i1 = -1; j1 = -1;
+            for (int i = 0; i < text.Length; i++)
+            {
+                shifr[k] = text[i];
+                for (int j = 0; j < ALPHA.Length; j++)
+                {
+                    if (text[i] == ALPHA[j])
+                    {
+                        i1 = j;
+                    }
+                    if (full_key[z] == ALPHA[j])
+                    {
+                        j1 = j;                   
+                    }
+                }
+                if (i1 != -1 && j1 != -1)
+                {
+                    i1 = i1 + j1;
+                    if (i1 >= 26)
+                        i1 = i1 % 26;
+                    shifr[k] = ALPHA[i1];
+
+                    z++; // next full_key
+                    if (z == full_key.Length)
+                        break;
+                }
+                i1 = -1; j1 = -1;
+                for (int j = 0; j < alpha.Length; j++)
+                {
+                    if (text[i] == alpha[j])
+                    {
+                        i1 = j;
+                    }
+                    if (full_key[z] == alpha[j])
+                    {
+                        j1 = j;
+                        
+                    }
+                }
+                if (i1 != -1 && j1 != -1)
+                {
+                    i1 = i1 + j1;
+                    if (i1 >= 26)
+                        i1 = i1 % 26;
+                    shifr[k] = alpha[i1];
+
+                    z++; // next full_key
+                    if (z == full_key.Length)
+                        break;
+                }
+                i1 = -1; j1 = -1;
+                for (int j = 0; j < ALPH.Length; j++)
+                {
+                    if (text[i] == ALPH[j])
+                    {
+                        i1 = j;
+                    }
+                    if (full_key[z] == ALPH[j])
+                    {
+                        j1 = j;
+                        
+                    }
+                }
+                if (i1 != -1 && j1 != -1)
+                {
+                    i1 = i1 + j1;
+                    if (i1 >= 33)
+                        i1 = i1 % 33;
+                    shifr[k] = ALPH[i1];
+
+                    z++; // next full_key
+                    if (z == full_key.Length)
+                        break;
+                }
+                i1 = -1; j1 = -1;
+                for (int j = 0; j < ALPH.Length; j++)
+                {
+                    if (text[i] == alph[j])
+                    {
+                        i1 = j;
+                    }
+                    if (full_key[z] == alph[j])
+                    {
+                        j1 = j;
+                        
+                    }
+                }
+                if (i1 != -1 && j1 != -1)
+                {
+                    i1 = i1 + j1;
+                    if (i1 >= 33)
+                        i1 = i1 % 33;
+                    shifr[k] = alph[i1];
+
+                    z++; // next full_key
+                    if (z == full_key.Length)
+                        break;
+                }
+                i1 = -1; j1 = -1;
+                k++;
+            }
+            string text1 = new string(shifr);
+            ShifrText.Text = text1;
+        }
+
+        private void button_unShifr_Click(object sender, EventArgs e)
+        {
+            char[] text = Text.Text.ToCharArray();
+            char[] shifr = Text.Text.ToCharArray();
+            char[] ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char[] alpha = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            char[] alph = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л',
+'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э',
+'ю', 'я' };
+            char[] ALPH = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray();
+            char[] key = keyBox.Text.ToCharArray();
+            int text_len = 0;
+            for (int i = 0; i < shifr.Length; i++)
+            {
+                for (int j = 0; j < ALPH.Length; j++)
+                {
+                    if (shifr[i] == ALPH[j] || shifr[i] == alph[j])
+                        text_len++;
+                }
+
+                for (int j = 0; j < ALPHA.Length; j++)
+                {
+                    if (shifr[i] == ALPHA[j] || shifr[i] == alpha[j])
+                        text_len++;
+                }
+            }
+
+            char[] full_key = new char[text_len];
+            int i1 = 0, j1 = 0;
+            for (int i = 0; i < text_len;) // дополняем ключ с проверкой на посторонние символы
+            {
+                for (int j = 0; j < ALPH.Length; j++)
+                {
+                    if (key[i1] == ALPH[j] || key[i1] == alph[j])
+                    {
+                        full_key[i] = key[i1];
+                        i++;
+                        break;
+                    }
+                }
+                for (int j = 0; j < ALPHA.Length; j++)
+                {
+                    if (key[i1] == ALPHA[j] || key[i1] == alpha[j])
+                    {
+                        full_key[i] = key[i1];
+                        i++;
+                        break;
+                    }
+                }
+                i1++;
+                if (i1 == key.Length)
+                    i1 = 0;
+            }
+
+            int k = 0, z = 0; i1 = -1; j1 = -1;
+            for (int i = 0; i < shifr.Length; i++)
+            {
+                text[k] = shifr[i];
+                for (int j = 0; j < ALPHA.Length; j++)
+                {
+                    if (shifr[i] == ALPHA[j])
+                    {
+                        i1 = j;
+                    }
+                    if (full_key[z] == ALPHA[j])
+                    {
+                        j1 = j;
+                    }
+                }
+                if (i1 != -1 && j1 != -1)
+                {
+                    i1 = i1 - j1;
+                    if (i1 < 0)
+                        i1 = i1 + 26;
+                    text[k] = ALPHA[i1];
+
+                    z++; // next full_key
+                    if (z == full_key.Length)
+                        break;
+                }
+                i1 = -1; j1 = -1;
+                for (int j = 0; j < alpha.Length; j++)
+                {
+                    if (shifr[i] == alpha[j])
+                    {
+                        i1 = j;
+                    }
+                    if (full_key[z] == alpha[j])
+                    {
+                        j1 = j;
+
+                    }
+                }
+                if (i1 != -1 && j1 != -1)
+                {
+                    i1 = i1 - j1;
+                    if (i1 < 0)
+                        i1 = i1 + 26;
+                    text[k] = alpha[i1];
+
+                    z++; // next full_key
+                    if (z == full_key.Length)
+                        break;
+                }
+                i1 = -1; j1 = -1;
+                for (int j = 0; j < ALPH.Length; j++)
+                {
+                    if (shifr[i] == ALPH[j])
+                    {
+                        i1 = j;
+                    }
+                    if (full_key[z] == ALPH[j])
+                    {
+                        j1 = j;
+
+                    }
+                }
+                if (i1 != -1 && j1 != -1)
+                {
+                    i1 = i1 - j1;
+                    if (i1 < 0)
+                        i1 = i1 + 33;
+                    text[k] = ALPH[i1];
+
+                    z++; // next full_key
+                    if (z == full_key.Length)
+                        break;
+                }
+                i1 = -1; j1 = -1;
+                for (int j = 0; j < ALPH.Length; j++)
+                {
+                    if (shifr[i] == alph[j])
+                    {
+                        i1 = j;
+                    }
+                    if (full_key[z] == alph[j])
+                    {
+                        j1 = j;
+
+                    }
+                }
+                if (i1 != -1 && j1 != -1)
+                {
+                    i1 = i1 - j1;
+                    if (i1 < 0)
+                        i1 = i1 + 33;
+                    text[k] = alph[i1];
+
+                    z++; // next full_key
+                    if (z == full_key.Length)
+                        break;
+                }
+                i1 = -1; j1 = -1;
+                k++;
+
+            }
+            string text1 = new string(text);
+            ShifrText.Text = text1;
+        }
+ 
+
+
+
+private void Text_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShifrText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
